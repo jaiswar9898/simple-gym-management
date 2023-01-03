b974ad967be29d70719ef7f9b13fc6ab9776a27b
jaiswar9898 <pranitajaiswal960@gmail.com>
2023-01-03 01:19:00 -0800

    gym

---
+++ sgapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Contact)
+admin.site.register(Enquiry)
+admin.site.register(Equipment)
+admin.site.register(Member)
+admin.site.register(Plan)
+
